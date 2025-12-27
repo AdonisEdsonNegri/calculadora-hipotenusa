1fdac135f8f4135cc1dd6ac6bea2cc8905b2a81d
AdonisEdsonNegri <adonis.edson.negri@terra.com.br>
2025-12-27 12:15:55 -0300

    Update calcular.js

--- api/calcular.js
+++ api/calcular.js
@@ -2,8 +2,12 @@ module.exports = (req, res) => {
   const lado1 = parseFloat(req.query.lado1);
   const lado2 = parseFloat(req.query.lado2);
   
+  if (isNaN(lado1) || isNaN(lado2)) {
+    return res.status(400).json({ error: 'Valores inválidos' });
+  }
+  
   const somaDosQuadrados = lado1 * lado1 + lado2 * lado2;
   const hipotenusa = Math.sqrt(somaDosQuadrados);
   
-  res.json({ hipotenusa: hipotenusa });
+  res.status(200).json({ hipotenusa: hipotenusa });
 };
